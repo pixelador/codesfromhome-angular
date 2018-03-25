073e2a694f9aca8c3bdb0ab6a2a05b2136ac843b
Erik Lopez <erik@eriklopez.work>
2018-03-25 17:45:45 -0500

    Adds mock posts data

---
+++ src/app/mock-posts.ts
@@ -0,0 +1,29 @@
+import { Post } from './post';
+
+export const POSTS: Post[] = [
+  {id: 11,
+    title: 'Post The First',
+    // tslint:disable-next-line:max-line-length
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec justo sit amet enim vulputate posuere ac quis lacus. Nullam tincidunt nisi eget condimentum luctus. Nunc nibh dui, sodales eget fringilla vitae, efficitur nec quam. Phasellus bibendum augue augue, vitae dapibus mauris pharetra sed. Nulla a lacinia quam. Nullam tristique odio et purus eleifend ultricies. Etiam non mi semper ligula interdum cursus.'
+  },
+  {id: 12,
+    title: 'Post The Second',
+    // tslint:disable-next-line:max-line-length
+    content: 'Vestibulum commodo nulla quis diam ornare dictum. Suspendisse sit amet placerat nisi. Quisque id aliquet velit, non venenatis nisl. Nunc neque mi, dignissim eget porta blandit, condimentum at justo. Ut condimentum accumsan orci et aliquam. Proin placerat ultrices volutpat. Morbi eu mauris vitae odio facilisis dignissim. Nunc quis nibh risus. Nunc lacinia iaculis mi. Aenean ac orci elit. Proin odio neque, tincidunt in magna in, interdum rhoncus ligula. Etiam mauris massa, mattis in nisi nec, dictum maximus tellus. Maecenas venenatis ante diam, vel rhoncus leo mattis at.'
+  },
+  {id: 13,
+    title: 'Post The Third',
+    // tslint:disable-next-line:max-line-length
+    content: 'Ut eu urna quis mauris ultricies rutrum dictum sed ex. Integer ex justo, maximus a porttitor in, sodales et nunc. Maecenas non commodo tellus. Nullam id luctus tortor. Praesent libero libero, imperdiet a lacinia ac, posuere vitae urna. Cras ut justo et justo mollis sagittis ut vel erat. Integer porttitor neque non odio blandit, nec fringilla odio posuere. Duis et sapien nisl.'
+  },
+  {id: 14,
+    title: 'Post The Fourth',
+    // tslint:disable-next-line:max-line-length
+    content: 'Fusce dignissim lacus sit amet pellentesque imperdiet. Mauris id justo eget ante porttitor mollis vitae at diam. Integer non lacus vitae tellus commodo convallis vel a quam. Donec vehicula, orci at semper lacinia, libero felis fermentum velit, sed placerat libero orci eu metus. Donec pulvinar ante semper risus ullamcorper sagittis. Morbi vitae turpis convallis, fringilla neque eu, rutrum ex. Curabitur sed velit tortor. '
+  },
+  {id: 15,
+    title: 'Post The Fifth',
+    // tslint:disable-next-line:max-line-length
+    content: 'Nam fringilla risus sed elit egestas, vel lobortis lectus facilisis. Nullam placerat felis tincidunt tortor sagittis, ut mollis orci dictum. Praesent mollis consequat nisl eu finibus. Donec et posuere velit. Nullam eget volutpat elit. Nam vel purus neque. Nulla vulputate risus a elit bibendum luctus. Cras neque mi, maximus vel enim quis, mattis maximus metus. Duis accumsan semper facilisis. Suspendisse fermentum nibh nec volutpat pharetra.'
+  },
+];
