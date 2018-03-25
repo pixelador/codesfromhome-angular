04bff0964cbdec88a4d18acb80d9a12b8f06ffb1
Erik Lopez <erik@eriklopez.work>
2018-03-25 15:35:13 -0500

    Declares Post and Footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { PostsComponent } from './posts/posts.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
