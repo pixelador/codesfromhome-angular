e198adeb7d70d31502ab4d6cbacfa3ecc3326e18
Erik Lopez <erik@eriklopez.work>
2018-03-25 17:47:40 -0500

    Adds default route, posts route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/posts', pathMatch: 'full'},
+  { path: '', component: PostsComponent},
+  { path: 'posts', component: PostsComponent},
+  { path: 'posts/:id', component: PostDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
