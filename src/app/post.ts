80ec029c4edc0e697788dff50b628555758502f6
Erik Lopez <erik@eriklopez.work>
2018-03-25 15:33:06 -0500

    Adds Post component

---
+++ src/app/post.ts
@@ -0,0 +1,8 @@
+export class Post {
+  id: number;
+  date: number;
+  title: string;
+  author: string;
+  content: string;
+  tags: string[];
+}
---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.scss']
+})
+export class PostsComponent implements OnInit {
+
+  title = 'Test Post #1';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
