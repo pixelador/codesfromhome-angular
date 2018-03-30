90ae13790b0680e23acb93dedfc7be5e5f698255
Erik Lopez <erik@eriklopez.work>
2018-03-30 16:01:10 -0500

    Adds location, route, post service

--- src/app/post-detail/post-detail.component.ts
+++ src/app/post-detail/post-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Post } from '../post';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-detail',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+    private location: Location
+  ) { }
+
+  @Input() post: Post;
 
   ngOnInit() {
+    this.getPost();
+  }
+
+  getPost(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    // + operator converts the string to a number
+    this.postService.getPost(id).subscribe(post => this.post = post);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
